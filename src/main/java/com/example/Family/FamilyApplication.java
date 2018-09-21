78b801d14702f90a4f5434009310a08d8566715a
luksen1991 <lukasz1991code@gmail.com>
2018-09-22 00:02:07 +0200

    Add ChildRepositoryTest findAll()

--- src/main/java/com/example/Family/FamilyApplication.java
+++ src/main/java/com/example/Family/FamilyApplication.java
@@ -26,35 +26,35 @@ public class FamilyApplication implements CommandLineRunner{
 		SpringApplication.run(FamilyApplication.class, args);
 	}
 
-	 @Autowired
-	 FamilyRepository familyRepository;
+//	 @Autowired
+//	 FamilyRepository familyRepository;
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		
-		Father father = new Father();
-		Child child1 = new Child();
-		Family family = new Family();
-
-		child1.setFirstName("Jan");
-		child1.setSecondName("Gwardian");
-		child1.setPesel("12345678910");
-		child1.setBirthDate(new Date());
-		child1.setSex("male");
-		child1.setFamily(family);
-
-		father.setFirstName("Jan");
-		father.setSecondName("Szpyt");
-		father.setBirthDate(new Date());
-		father.setPesel("9999");
-		father.setFamily(family);
-
-		List<Child> listChild = new ArrayList<Child>();
-		listChild.add(child1);
-
-		family.setFather(father);
-		family.setChildList(listChild);
-		familyRepository.addFamily(family);
+//
+//		Father father = new Father();
+//		Child child1 = new Child();
+//		Family family = new Family();
+//
+//		child1.setFirstName("Jan");
+//		child1.setSecondName("Gwardian");
+//		child1.setPesel("12345678910");
+//		child1.setBirthDate(new Date());
+//		child1.setSex("male");
+//		child1.setFamily(family);
+//
+//		father.setFirstName("Jan");
+//		father.setSecondName("Szpyt");
+//		father.setBirthDate(new Date());
+//		father.setPesel("9999");
+//		father.setFamily(family);
+//
+//		List<Child> listChild = new ArrayList<Child>();
+//		listChild.add(child1);
+//
+//		family.setFather(father);
+//		family.setChildList(listChild);
+//		familyRepository.addFamily(family);
 		
 		
 	}
---
+++ src/test/java/com/example/Family/Repository/ChildRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.example.Family.Repository;
+
+import com.example.Family.Entities.Child;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = ChildRepository.class)
+public class ChildRepositoryTest {
+
+    @Autowired
+    ChildRepository childRepository;
+
+    @Test
+    public void testFindAllChildren(){
+        int expectedValue =13;
+        List<Child> listChild = childRepository.findAllChild();
+        System.out.println("Val: "+listChild.size());
+        assertEquals(expectedValue,listChild.size());
+    }
+}

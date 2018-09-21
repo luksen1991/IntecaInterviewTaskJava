8d52a494ac1d324688135a5c64cd53545a544897
luksen1991 <lukasz1991code@gmail.com>
2018-09-22 00:06:22 +0200

    Add FatherRepositoryTest findAll()

---
+++ src/test/java/com/example/Family/Repository/FatherRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.example.Family.Repository;
+
+import com.example.Family.Entities.Father;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = FatherRepository.class)
+public class FatherRepositoryTest {
+
+    @Autowired
+    private FatherRepository fatherRepository;
+
+    @Test
+    public void findAllFathers(){
+        int expectedValue = 12;
+        List<Father> fatherList = fatherRepository.findAllFathers();
+        assertEquals(expectedValue,fatherList.size());
+
+    }
+
+}
